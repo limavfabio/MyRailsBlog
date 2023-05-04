ef1ad7629ba096bbbd6ab7de7ee3a1f37694217b
limavfabio <limavfabio@gmail.com>
2023-05-04 15:17:25 -0300

    Make seed creation error on failure

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # Create 10 users with randomized data
 10.times do
-  User.create(
+  User.create!(
     name: Faker::Name.name,
     photo: Faker::Avatar.image,
-    bio: Faker::Lorem.sentence
+    bio: Faker::Lorem.sentence(word_count: 3),
   )
 end
 
 # Create 50 posts with randomized data
 50.times do
-  Post.create(
+  Post.create!(
     title: Faker::Lorem.sentence(word_count: 3),
     text: Faker::Lorem.paragraph,
     author: User.order(Arel.sql('RANDOM()')).first
@@ -18,7 +18,7 @@ end
 
 # Create 100 comments with randomized data
 100.times do
-  Comment.create(
+  Comment.create!(
     text: Faker::Lorem.sentence,
     author: User.order(Arel.sql('RANDOM()')).first,
     post: Post.order(Arel.sql('RANDOM()')).first
@@ -27,7 +27,7 @@ end
 
 # Create 200 likes with randomized data
 200.times do
-  Like.create(
+  Like.create!(
     author: User.order(Arel.sql('RANDOM()')).first,
     post: Post.order(Arel.sql('RANDOM()')).first
   )
