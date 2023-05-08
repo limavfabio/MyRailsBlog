1abcc2f86b4eb7484983a042440416a80623c9e9
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-08 17:53:24 -0300

    Move root page to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/show'
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'home#index'
+  root 'users#index'
+  get '/users', to: 'users#index', as: 'users'
   get 'user/:user_id', to: 'users#show', as: :user
   get 'user/:user_id/posts', to: 'posts#index', as: :user_posts
   get 'user/:user_id/posts/:id', to: 'posts#show', as: :user_post
