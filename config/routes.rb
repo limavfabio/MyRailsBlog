f8afddd3532e96ea9882c890549a08bbe0f67e23
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-12 17:18:18 -0300

    Update routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root 'users#index'
-
-  resources :users do
-    resources :posts do
-      post 'create_comment', on: :member
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create, :destroy]
     end
   end
+  root 'users#index'
 end
