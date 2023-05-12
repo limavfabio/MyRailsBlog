71ca4a4787a4f8cdafeb67f74ea8db81ad25bd1d
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-12 17:16:46 -0300

    Move comment logic to it's own controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,41 +11,21 @@ class PostsController < ApplicationController
   end
 
   def new
-    @current_user = User.first
     @post = Post.new
   end
 
   def create
-    @current_user = User.first
     @post = Post.new(
-      title: params[:post][:text],
+      title: params[:post][:title],
       text: params[:post][:text],
-      author: @current_user
+      author: current_user
     )
 
     if @post.save
-      redirect_to user_post_path(@current_user, @post)
+      redirect_to user_post_path(@post.author, @post)
     else
       flash[:error] = 'Post could not be saved'
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def create_comment
-    @current_user = User.first
-
-    post = Post.find(params[:id])
-    comment = Comment.new(
-      text: params[:comment][:text],
-      author: @current_user,
-      post:
-    )
-
-    if comment.save
-      redirect_to user_post_path(post.author, post)
-    else
-      flash[:error] = 'Comment could not be saved'
-      render :new, status: :unprocessable_entity
+      redirect_to user_posts_path(@post.author)
     end
   end
 
@@ -53,9 +33,5 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :text)
-  end
-
-  def comment_params
-    params.require(:comment).permit(:text)
   end
 end
